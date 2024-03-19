ccf71937cf89f78299d29cfc1f8428d975a3f587
suhwan1 <wistlin2@gmail.com>
2024-03-19 18:11:54 +0900

    practice_basic.sql

--- practice_basic.sql
+++ practice_basic.sql
@@ -8,3 +8,8 @@ select * from Books ORDER BY price DESC limit 12;
 
 select * from `Categories` where category_name='과학';
 select email,age from `Users` where age>=28 and age<=30;
+
+select year(pub_year),count(*) from `Books` GROUP BY YEAR(pub_year) order by count(*) desc;
+
+select * from `Purchases` where quantity>4 limit 12;
+select title,category_name from Books,Categories limit 12;
